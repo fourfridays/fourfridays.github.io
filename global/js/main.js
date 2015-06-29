c532429cd636fb644a2ac63ea60e15ed07dda3d5
Umair Abbasi <umair.abbasi@fourfridays.com>
2015-06-28 19:47:39 -0500

    Added scotchpanel.js

---
+++ global/js/main.js
@@ -0,0 +1,12 @@
+$('#scotch-panel').scotchPanel({
+  containerSelector: 'body',
+  direction: 'right',
+  duration: 300,
+  transition: 'ease',
+  clickSelector: '.toggle-panel',
+  distanceX: '70%',
+  enableEscapeKey: true
+});
+                
+
+                
